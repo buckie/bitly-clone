6ea7d9efd187975fb5f9c19861293b5308779bb5
buckie <wjmartino@gmail.com>
2015-02-01 01:00:07 -0500

    started adding the react front end... got bored
    Signed-off-by: buckie <wjmartino@gmail.com>

--- src/Main.hs
+++ src/Main.hs
@@ -6,11 +6,6 @@ import BasePrelude
 import Hitly.Server
 import Hitly.Types
 import Hitly.Hash
--- main :: IO ()
--- main = do args <- getArgs
---           case length args of
---                1 ->  SL8.putStrLn $ mkLink $ args !! 0
---                _ ->  error "Program takes only 0 or 1 argument"
 
 main :: IO ()
 main = runServer
